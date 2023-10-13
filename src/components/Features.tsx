c64e1b13c96242d656dfeb81343f37488485ab74
Thomas Groutars <tgroutars@gmail.com>
2023-10-13 11:16:29 +0200

    add features

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -7,6 +7,8 @@ import {
   Text,
   Title,
 } from '@mantine/core';
+import { useFeature } from '@unrevealed/react';
+import { features } from '~/generated/unrevealed';
 
 const useStyles = createStyles((theme) => ({
   title: {
@@ -55,6 +57,14 @@ const useStyles = createStyles((theme) => ({
 export function Features() {
   const { classes } = useStyles();
 
+  const like = useFeature('like');
+  const dislike = useFeature('dislike');
+  const superlike = useFeature('superlike');
+  const boost = useFeature('boost');
+  const unlimitedLikes = useFeature('unlimited-likes');
+  const viewLikes = useFeature('view-likes');
+  const instagram = useFeature('instagram');
+
   return (
     <Container size="lg" py="xl">
       <Title order={2} className={classes.title} ta="center" mt="sm"></Title>
@@ -65,24 +75,27 @@ export function Features() {
         mt={50}
         breakpoints={[{ maxWidth: 'md', cols: 1 }]}
       >
-        {/* {workflow.enabled && (
-          <Feature feature={features['workflow']} classes={classes} />
+        {like.enabled && (
+          <Feature feature={features['like']} classes={classes} />
         )}
-        {codeGeneration.enabled && (
-          <Feature feature={features['code-generation']} classes={classes} />
+        {dislike.enabled && (
+          <Feature feature={features['dislike']} classes={classes} />
         )}
-        {sdks.enabled && (
-          <Feature feature={features['sdks']} classes={classes} />
+        {superlike.enabled && (
+          <Feature feature={features['superlike']} classes={classes} />
         )}
-        {vscodeExtension.enabled && (
-          <Feature feature={features['vscode-extension']} classes={classes} />
+        {boost.enabled && (
+          <Feature feature={features['boost']} classes={classes} />
         )}
-        {peopleView.enabled && (
-          <Feature feature={features['people-view']} classes={classes} />
+        {unlimitedLikes.enabled && (
+          <Feature feature={features['unlimited-likes']} classes={classes} />
         )}
-        {partialRollouts.enabled && (
-          <Feature feature={features['partial-rollouts']} classes={classes} />
-        )} */}
+        {viewLikes.enabled && (
+          <Feature feature={features['view-likes']} classes={classes} />
+        )}
+        {instagram.enabled && (
+          <Feature feature={features['instagram']} classes={classes} />
+        )}
       </SimpleGrid>
     </Container>
   );
--- src/generated/unrevealed.ts
+++ src/generated/unrevealed.ts
@@ -2,7 +2,13 @@ import type { FeatureKey } from '@unrevealed/react';
 
 declare module '@unrevealed/react' {
   interface Features {
-    'dark-mode': boolean;
+    boost: boolean;
+    dislike: boolean;
+    instagram: boolean;
+    like: boolean;
+    superlike: boolean;
+    'unlimited-likes': boolean;
+    'view-likes': boolean;
   }
 
   interface UserTraits {
@@ -20,12 +26,42 @@ export interface Feature {
 }
 
 export const features: Record<FeatureKey, Feature> = {
-  'dark-mode': {
-    name: "Dark mode",
-    description: "",
+  boost: {
+    name: "Boost",
+    description: "Boost your profile to get in front of everyone",
+  },
+  dislike: {
+    name: "Dislike",
+    description: "Swipe left to dislike",
+  },
+  instagram: {
+    name: "Link instagram account",
+    description: "Show instagram account on your profile",
+  },
+  like: {
+    name: "Like",
+    description: "Swipe right to like",
+  },
+  superlike: {
+    name: "Superlike",
+    description: "Send super likes to others",
+  },
+  'unlimited-likes': {
+    name: "Unlimited likes",
+    description: "Unlock unlimited likes",
+  },
+  'view-likes': {
+    name: "View who likes you",
+    description: "Unlock profiles who liked you",
   },
 };
 
 export const featureKeys: FeatureKey[] = [
-  'dark-mode',
+  'boost',
+  'dislike',
+  'instagram',
+  'like',
+  'superlike',
+  'unlimited-likes',
+  'view-likes',
 ];
